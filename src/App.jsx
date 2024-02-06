04ea441a6c968ba6cee39def3e64a7348bcf3ff6
alihassan8989 <maqsoodia777@gmail.com>
2024-02-06 15:56:42 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Form from './Compoents/Form';
 import Navbar from './Compoents/Navbar'
-import Form from './Compoents/Form'
 
 const App = () => {
-  const [mode, setMode] = useState('light')
+  const [mode, setMode] = useState('light');
 
   const toggle = () => {
     if (mode === 'light') {
-      setMode('dark')
-      document.body.style.background = '#000000'
+      setMode('dark');
+      document.body.style.background = '#000000';
     } else {
-      setMode('light')
-      document.body.style.background = 'white'
+      setMode('light');
+      document.body.style.background = 'white';
     }
-  }
+  };
+
 
   return (
+
+
     <>
       <Navbar mode={mode} toggle={toggle} />
       <Form mode={mode} />
-
-
     </>
   )
-}
+};
 
-export default App
+export default App;
