6c56d5ef1a0498d60c35b4ec2936f39868f2999a
HassanThePassionate <maqsoodia777@gmail.com>
2024-05-15 16:35:34 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import Form from './Compoents/Form';
-import Navbar from './Compoents/Navbar'
+import React, { useState } from "react";
+import Form from "./Compoents/Form";
+import Navbar from "./Compoents/Navbar";
+import Ad from "./Compoents/Ad";
 
 const App = () => {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
   const toggle = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.background = '#000000';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.background = "#000000";
     } else {
-      setMode('light');
-      document.body.style.background = 'white';
+      setMode("light");
+      document.body.style.background = "white";
     }
   };
 
-
   return (
-
-
     <>
       <Navbar mode={mode} toggle={toggle} />
+      <Ad />
       <Form mode={mode} />
+      <Ad />
     </>
-  )
+  );
 };
 
 export default App;
---
+++ src/Compoents/Ad.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Ad = () => {
+  return <div className='ads'>google Ad</div>;
+};
+
+export default Ad;
